d89ee5f2381d60f5b25e2e979f6339b47c612be4
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-11-29 11:13:20 -0500

    FEAT: Add views for authorization

--- src/models/Product.java
+++ src/models/Product.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication6;
+package models;
 
 /**
  *
  * @author kpalmall
  */
-public class JavaApplication6 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Product {
     
 }
