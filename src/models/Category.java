e83b1c833614db85a0c8601b77a3689a48875d8f
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-11-30 17:00:28 -0500

    FEAT: Add class category.

--- src/models/Category.java
+++ src/models/Category.java
@@ -10,5 +10,52 @@ package models;
  * @author kpalmall
  */
 public class Category {
-    
+
+    private int id;
+    private String name;
+    private Boolean state;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+        this.state = true;
+    }
+
+    public Category(String name, Boolean state) {
+        this.name = name;
+        this.state = state;
+    }
+
+    public Category(int id, String name, Boolean state) {
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
 }
