6f5e949a027f84828cce684ec442c771e93664c3
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-11-30 16:50:48 -0500

    FEAT: Add operations for gestor db.

--- src/NewMain.java
+++ src/NewMain.java
@@ -1,5 +1,4 @@
 
-import util.GestorBd;
 
 
 /*
--- src/util/GestorBd.java
+++ src/util/GestorBd.java
@@ -6,14 +6,17 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author kpalmall
  */
-public class GestorBd {
-    
+public class GestorBd { 
     
     public static String execute(String sql) {
         String m = "Ejecucion realizada correctamente.";
@@ -30,6 +33,43 @@ public class GestorBd {
             m = e.getMessage();
         }
         return m;
+    }
+    
+    public static Object[] buscar(String sql){
+        Object[] fila = null;
+        List lista= listar(sql);
+        if(lista!=null){
+            if(lista.size()>1){
+                fila = (Object[])lista.get(1);
+            }
+        }
+        return fila;
+    }
+    
+    
+    public static List listar(String sql){
+        List lista = new ArrayList<>();
+        try {
+            Connection cn = Connectionn.getConexion();
+            if (cn!=null) {
+                Statement st = cn.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                ResultSetMetaData rm = rs.getMetaData();
+                int numCol = rm.getColumnCount();
+                
+                while (rs.next()) {                    
+                    Object[] fila = new Object[numCol];
+                    for (int i = 0; i < numCol; i++)fila[i] = rs.getObject(i+1);
+                    lista.add(fila);
+                }
+                cn.close();
+            }else{
+                lista = null;
+            }
+        } catch (Exception e) {
+            lista = null;
+        }
+        return lista; 
     }
     
 }
