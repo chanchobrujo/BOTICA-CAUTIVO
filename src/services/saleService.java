38d3cab664bdca6cb8a945a6d7c4be4878359872
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-12-14 22:22:01 -0500

    FEAT: Add service for sales.

---
+++ src/services/saleService.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.Product;
+import entities.Sale;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author chanchobrujo
+ */
+public class saleService {
+    private userService userService;
+    private productService productService;
+    
+    private Sale sale = new Sale();
+
+    public saleService() {
+        userService = new userService();
+        productService = new productService();
+    } 
+    
+    public Sale newSale(int idUser, Double porDes){
+        this.sale.setUser(userService.findById(idUser));
+        this.sale.setPordes(porDes);
+        
+        return this.sale;
+    }
+    
+    public void removeProduct(int id){
+        this.sale.removeProduct(id);
+    }
+    
+    public void addLine(int id, int cantidad) {
+        this.sale.addProduct(productService.findById(id).get(), cantidad);
+    }
+    
+    public List<Product> findAllProducts() {
+        return productService.findAll().stream()
+                .filter(Product::getState)
+                .collect(Collectors.toList());
+    }
+    
+    public void clearCart() {
+        this.sale.clearCart();
+    }
+
+}
