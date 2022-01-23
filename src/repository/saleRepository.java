ababc46906b54c70047e4c3dd3a9fc9b3d59c0f8
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-23 16:59:55 -0500

    FEAT: Mapper for sale and funtions.

--- src/repository/saleRepository.java
+++ src/repository/saleRepository.java
@@ -5,20 +5,60 @@
  */
 package repository;
 
+import com.jgoodies.common.base.Objects;
 import entities.Sale;
 import enums.AlertMessage;
+import enums.Constans;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import model.ModelSale;
 import util.Commons;
 import util.GestorBd;
+import util.Mapper;
 
 /**
  *
  * @author chanchobrujo
  */
-public class saleRepository {  
+public class saleRepository {
+
+    private customerRepository customerRepository;
     private detailsRepository detailsRepository;
-    
-    public saleRepository(){
+    private userRepository userRepository;
+
+    public saleRepository() {
+        userRepository = new userRepository();
         detailsRepository = new detailsRepository();
+        customerRepository = new customerRepository();
+    }
+
+    private String UserNameById(String id) {
+        return this.userRepository.findAll().stream()
+                .filter(user -> Objects.equals(user.getId(), Commons.StringToInteger(id)))
+                .map(mapper -> {
+                    return mapper.getFirtsname()
+                            .concat(Constans.space)
+                            .concat(mapper.getLastname())
+                            .concat(Constans.double_point)
+                            .concat(id);
+                })
+                .findFirst()
+                .get();
+    }
+
+    private String CustomerNameById(String id) {
+        return this.customerRepository.findAll().stream()
+                .filter(customer -> Objects.equals(customer.getId(), Commons.StringToInteger(id)))
+                .map(mapper -> {
+                    return mapper.getFirtsname()
+                            .concat(Constans.space)
+                            .concat(mapper.getLastname())
+                            .concat(Constans.double_point)
+                            .concat(id);
+                })
+                .findFirst()
+                .get();
     }
 
     public String grabarVentasParaUsuarioCliente(Sale sale) {
@@ -35,7 +75,29 @@ public class saleRepository {
                 + " " + sale.getCustomer().getId() + ")";
         GestorBd.execute(sql);
         this.detailsRepository.grabarDetalles(sale.getCart());
-        
+
         return AlertMessage.EXECUTE_SUCCESS.getValue();
+    }
+
+    public List<ModelSale> findAll() {
+        List list = GestorBd.findAll("SELECT * FROM sale;");
+        List<ModelSale> findAll = new ArrayList<>();
+
+        if (Commons.collectionNonEmptyOrNull(list)) {
+            list.stream()
+                    .map(mapper -> {
+                        ModelSale modelSale = Mapper.mapperSale(mapper);
+                        Object[] row = (Object[]) mapper;
+
+                        modelSale.setUser(this.UserNameById(row[7].toString()));
+                        modelSale.setCustomer(this.CustomerNameById(row[8].toString()));
+
+                        findAll.add(modelSale);
+                        return mapper;
+                    })
+                    .collect(Collectors.toList());
+        }
+
+        return findAll;
     }
 }
