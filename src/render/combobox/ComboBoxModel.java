03fe6c426e9480e7ec9287964d73196f01b1b705
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-26 18:48:48 -0500

    FEAT: Init combo model class.

---
+++ src/render/combobox/ComboBoxModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package render.combobox;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author kpalmall
+ */
+public class ComboBoxModel {
+    
+}
